4e7a427185b1045d8f384e696a4bac67857f6f71
Olin Kirkland <olin.kirkland@beg.de>
2024-06-18 16:52:11 +0200

    Scaffold character sheet

--- src/pages/sheets/TheCharacterSheet.vue
+++ src/pages/sheets/TheCharacterSheet.vue
@@ -12,6 +12,7 @@
           <h1>CHARACTER TYPE DESCRIPTION</h1>
           <h1>CREW</h1>
         </section>
+        <Divider />
         <section>
           <h1>NAME</h1>
           <h1>ALIAS</h1>
@@ -21,36 +22,43 @@
           <h1>BACKGROUND</h1>
           <h1>BACKGROUND DESCRIPTION</h1>
         </section>
-      </div>
-      <div>
+        <Divider />
         <section>
           <h1>VICE</h1>
           <h1>VICE PURVEYOR</h1>
         </section>
+        <Divider />
         <section>
           <h1>CONTACTS</h1>
         </section>
+      </div>
+      <div>
         <section>
           <h1>STRESS + MAX STRESS</h1>
           <h1>TRAUMAS + MAX TRAUMAS</h1>
         </section>
+        <Divider />
         <section>
           <h1>HARMS</h1>
           <h1>HEALING</h1>
           <h1>ARMORS</h1>
         </section>
+        <Divider />
         <section>
           <h1>PLAYBOOK XP</h1>
           <h1>SPECIAL ABILITIES</h1>
         </section>
+        <Divider />
         <section>
           <h1>INSIGHT XP</h1>
           <h1>HUNT, STUDY, SURVEY, TINKER</h1>
         </section>
+        <Divider />
         <section>
           <h1>PROWESS XP</h1>
           <h1>FINESSE, PROWL, SKIRMISH, WRECK</h1>
         </section>
+        <Divider />
         <section>
           <h1>RESOLVE XP</h1>
           <h1>ATTUNE, COMMAND, CONSORT, SWAY</h1>
